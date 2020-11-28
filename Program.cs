c72e0eb6a8f23c58fada0b4632fdd4e635d6ae36
Polinnn <75161265+Polinnn@users.noreply.github.com>
2020-11-28 12:59:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = Convert.ToInt32("5345"); //5345
+            int n2 = Convert.ToInt32(Console.ReadLine()); // 55
+
+            int a = n1 + n2;
+
+            Console.WriteLine(a);
+            Console.ReadKey();
+
+
+        }
+    }
+}
