4338ea09cf41401a5e38ff66ef75b40be707bde9
Polinnn <75161265+Polinnn@users.noreply.github.com>
2020-11-28 13:06:02 +0300

    Add files via upload

---
+++ vzeeee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            int n2 = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine(n1 + n2);
+            Console.WriteLine(n1 - n2);
+            Console.WriteLine(n1 * n2);
+            Console.WriteLine(n1 /n2);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
